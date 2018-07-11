48412c446bda54b91fc43df476844884c4c5ccad
Unknown <aungko.akkt@gmail.com>
2018-07-11 15:41:20 +0630

    Auto stash before merge of "master" and "origin/master"

---
+++ SuHtar/app/src/main/java/com/example/aungko/suhtar/splash_screen_process/SplashScreenActivity.java
@@ -0,0 +1,27 @@
+package com.example.aungko.suhtar.splash_screen_process;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.aungko.suhtar.MainActivity;
+import com.example.aungko.suhtar.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.slider_1);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+                finish();
+            }
+        },1000);
+    }
+}
