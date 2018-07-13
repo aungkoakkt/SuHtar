3d970330c675eb9fa15508dd1ec4d60752cce35f
Unknown <aungkoko015726@gmail.com>
2018-07-13 10:06:13 +0630

    Myanmar font embedded attach with wallet fragment

---
+++ SuHtar/app/src/main/java/com/example/aungko/suhtar/MainApplication.java
@@ -0,0 +1,30 @@
+package com.example.aungko.suhtar;
+
+import android.app.Application;
+
+import com.example.aungko.suhtar.helper.FontEmbedder;
+import com.example.aungko.suhtar.helper.Moulder;
+import com.example.aungko.suhtar.helper.TypefaceManager;
+
+import me.myatminsoe.mdetect.MDetect;
+
+public class MainApplication extends Application {
+
+    public static TypefaceManager typefaceManager;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        MDetect.INSTANCE.init(this);
+
+        typefaceManager = new TypefaceManager(getAssets());
+
+        if (MDetect.INSTANCE.isUnicode()){
+            FontEmbedder.init(typefaceManager.getShitUnicode());
+            Moulder.init(true);
+        }else {
+            FontEmbedder.init(typefaceManager.getShitZawgyi());
+            Moulder.init(false);
+        }
+    }
+}
--- SuHtar/app/src/main/java/com/example/aungko/suhtar/wallet_process/WalletFragment.java
+++ SuHtar/app/src/main/java/com/example/aungko/suhtar/wallet_process/WalletFragment.java
@@ -3,19 +3,56 @@ package com.example.aungko.suhtar.wallet_process;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.aungko.suhtar.R;
+import com.example.aungko.suhtar.helper.FontEmbedder;
 
 public class WalletFragment extends Fragment {
+
+
+    public Toolbar toolbar;
+    public TextView toolText,InitVal,OwnBudget,FloatingVal;
+    public EditText EnterVal;
+    public TabLayout tabLayout;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.wallet,container,false);
+        toolbar = view.findViewById(R.id.tb_budget);
+        toolText = view.findViewById(R.id.toolbar_title);
+        InitVal = view.findViewById(R.id.initial_value);
+        EnterVal = view.findViewById(R.id.one_hundred_thousand);
+        OwnBudget = view.findViewById(R.id.own_budget);
+        FloatingVal = view.findViewById(R.id.floating_amount);
+        tabLayout = view.findViewById(R.id.tabLayout);
+
+/*        tabItemIncome = view.findViewById(R.id.income_category);
+        tabItemExpend = view.findViewById(R.id.expend_category);*/
         return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        FontEmbedder.forceTitle(toolText, getString(R.string.wallet));
+        FontEmbedder.forceTitle(InitVal, getString(R.string.initial_value));
+        FontEmbedder.EditTextforce(EnterVal, getString(R.string.one_hundred_thousand), getString(R.string.one_hundred_thousand));
+        FontEmbedder.forceTitle(OwnBudget, getString(R.string.own_budget));
+        FontEmbedder.forceTitle(FloatingVal, getString(R.string.floating_amount));
+
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.income_category));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.expend_category));
+
     }
 }
